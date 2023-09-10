2a52c7344a39e395354556e3764b85a30656c16b
waffel183 <koensanderse2000@gmail.com>
2023-09-10 17:41:13 +0200

    add increasing and decreasing functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 import sqlite3
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -18,32 +19,14 @@ def add_new_skill(skill_name, skill_desc, skill_level):
     connection.commit()
     connection.close()
 
-def improve_skill(skill_name):
-    connection = sqlite3.connect('database.db')
-    cur = connection.cursor()
-
-    # Add 1 to the skill_level of the skill_name row
-
-    connection.commit()
-    connection.close()
-
-def decrease_skill(skill_name):
-    connection = sqlite3.connect('database.db')
-    cur = connection.cursor()
-
-    # remove 1 to the skill_level of the skill_name row
-
-    connection.commit()
-    connection.close()
-
 # For testing:
 #add_new_skill('hacking', 'skill about hacking', '10')
 
 @app.route("/new_skill/", methods=('GET', 'POST'))
 def new_skill():
     if request.method == 'POST':
-        skill_name = request.form['skill_name']
-        skill_desc = request.form['skill_desc']
+        skill_name = urllib.parse.quote(request.form['skill_name'],safe='')
+        skill_desc = urllib.parse.quote(request.form['skill_desc'],safe='')
         skill_level = request.form['skill_level']
 
         if not skill_name:
@@ -58,6 +41,41 @@ def new_skill():
             return redirect(url_for('skills'))
 
     return render_template('new_skill.html')
+
+@app.route("/decrease/<skill_name>", methods=['POST'])
+def decrease_skill(skill_name):
+    if request.method == 'POST':
+        skill_name = urllib.parse.quote(skill_name, safe='')
+
+        connection = sqlite3.connect('database.db')
+        cur = connection.cursor()
+
+        # remove 1 to the skill_level of the skill_name row
+        # TODO: Add make sure that it cannot be lower than 0
+        query = "UPDATE skills SET skill_level = skill_level - 1 WHERE skill_name = '" + skill_name + "'"
+        print(query)
+        cur.execute(query)
+
+        connection.commit()
+        connection.close()
+        return redirect(url_for('skills'))
+
+@app.route("/increase/<skill_name>", methods=['POST'])
+def increase_skill(skill_name):
+    skill_name = urllib.parse.quote(skill_name, safe='')
+    if request.method == 'POST':
+
+        connection = sqlite3.connect('database.db')
+        cur = connection.cursor()
+
+        # Add 1 to the skill_level of the skill_name row
+        query = "UPDATE skills SET skill_level = skill_level + 1 WHERE skill_name = '" + skill_name + "'"
+        print(query)
+        cur.execute(query)
+
+        connection.commit()
+        connection.close()
+        return redirect(url_for('skills'))
 
 @app.route("/")
 def skills():
